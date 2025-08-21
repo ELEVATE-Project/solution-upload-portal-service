b9b263b78b44505b29ad4056e445edd252b88fbb
thippeswamy12 <108321976+thippeswamy12@users.noreply.github.com>
2025-08-21 19:00:01 +0530

    Update project.py

--- backend/src/main/modules/project.py
+++ backend/src/main/modules/project.py
@@ -1249,7 +1249,7 @@ class Elevateproject:
                                                 #  data=json.dumps(config.get(environment, 'keyclockAPIBody')))
             # Elevateproject.terminatingMessage(type(json.loads(config.get(environment, 'keyclockAPIBody'))))\
             loginBody = {
-                'email' : identifier,
+                'identifier' : identifier,
                 'password' : password
             }
             responseKeyClockUser = requests.post(userLoginHost + keyclockapiurl , headers=headerKeyClockUser, data=loginBody)
