96957ccd4f685bed748be2978928c96a17a29982
bharathRajaSL <bharathr@shikshalokam.org>
2025-10-09 13:27:26 +0530

    Sample Templates

--- backend/src/main/modules/config.py
+++
@@ -1,8 +0,0 @@
-connectionUrl= "" #add mongo url
-databaseName = "templateValidationSup"
-collectionName = "validation"
-conditionCollection = "conditions"
-hostUrl = "https://staging.sunbirded.org/"
-preprodHostUrl = "https://staging.sunbirded.org/"
-errBasic = 300
-errAdv = 301
