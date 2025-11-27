04a5bf5b78b299f8a302353eb059bc59aca43126
Bharath Raja <bharathr@shikshalokam.org>
2025-11-27 17:58:58 +0530

    Project with Certificate for Observation LED IMP

--- backend/src/main/modules/project.py
+++ backend/src/main/modules/project.py
@@ -1312,7 +1312,7 @@ class Elevateproject:
         messageArr.append("Upload status code : " + str(responseFetchSolutionApiUrl.status_code))
         Elevateproject.createAPILog(solutionName_for_folder_path, messageArr)
         print(responseFetchSolutionApiUrl.text,"responseFetchSolutionApiUrl")
-        zz# print(responseFetchSolutionJson)
+        # print(responseFetchSolutionJson)
         if responseFetchSolutionApiUrl.status_code == 200:
             print('Fetch solution Api Success')
             solutionName = responseFetchSolutionJson["result"]["name"]
